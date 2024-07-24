8d47514e4e54b7b07032af1fd85a6a19bedc0a34
Ryan Nguyen <rnguye65@calpoly.edu>
2024-07-24 10:56:23 -0700

    moved things again

--- src/FinalProj/access/EmployeeAccess.java
+++ src/FinalProj/access/EmployeeAccess.java
@@ -3,8 +3,6 @@ package FinalProj.access;
 import FinalProj.DatabaseConnection;
 import FinalProj.classes.Employee;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EmployeeAccess{
 
@@ -24,43 +22,43 @@ public class EmployeeAccess{
     }
 
     // select all employees
-    public List<Employee> getAllEmployees(){
-        String selectStatement = "select * from Employees";
-        List<Employee> employees = new ArrayList<>();
-        try(Connection conn = DatabaseConnection.connect(); // connect to database
-        Statement stmt = conn.createStatement();
-        ResultSet dataResult = stmt.executeQuery(selectStatement)){
-            while(dataResult.next()){
-                Employee employee = new Employee(
-                dataResult.getInt("employee_id"),
-                dataResult.getString("first_name"),
-                dataResult.getString("last_name"),
-                dataResult.getString("phone_number"),
-                dataResult.getInt("employee_type")
-                );
-                employees.add(employee);
-            }
-        }catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }
-        return employees;
-    }
+    // public List<Employee> getAllEmployees(){
+    //     String selectStatement = "select * from Employees";
+    //     List<Employee> employees = new ArrayList<>();
+    //     try(Connection conn = DatabaseConnection.connect(); // connect to database
+    //     Statement stmt = conn.createStatement();
+    //     ResultSet dataResult = stmt.executeQuery(selectStatement)){
+    //         while(dataResult.next()){
+    //             Employee employee = new Employee(
+    //             dataResult.getInt("employee_id"),
+    //             dataResult.getString("first_name"),
+    //             dataResult.getString("last_name"),
+    //             dataResult.getString("phone_number"),
+    //             dataResult.getInt("employee_type")
+    //             );
+    //             employees.add(employee);
+    //         }
+    //     }catch(SQLException ex){
+    //         System.out.println(ex.getMessage());
+    //     }
+    //     return employees;
+    // }
 
     // Method to update an existing employee
-    public void updateEmployee(Employee employee){
-        String updateStatement = "update Employees set first_name = ?, last_name = ?, phone_number = ?, employee_type = ? where employee_id = ?";
-        try(Connection conn = DatabaseConnection.connect(); // connect to database
-            PreparedStatement statement = conn.prepareStatement(updateStatement)){
-                statement.setString(1, employee.getFirstName());
-                statement.setString(2, employee.getLastName());
-                statement.setString(3, employee.getPhoneNumber());
-                statement.setInt(4, employee.getEmpType());
-                statement.setInt(5, employee.getEmployeeId());
-                statement.executeUpdate();
-        }catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
+    // public void updateEmployee(Employee employee){
+    //     String updateStatement = "update Employees set first_name = ?, last_name = ?, phone_number = ?, employee_type = ? where employee_id = ?";
+    //     try(Connection conn = DatabaseConnection.connect(); // connect to database
+    //         PreparedStatement statement = conn.prepareStatement(updateStatement)){
+    //             statement.setString(1, employee.getFirstName());
+    //             statement.setString(2, employee.getLastName());
+    //             statement.setString(3, employee.getPhoneNumber());
+    //             statement.setInt(4, employee.getEmpType());
+    //             statement.setInt(5, employee.getEmployeeId());
+    //             statement.executeUpdate();
+    //     }catch(SQLException ex){
+    //         System.out.println(ex.getMessage());
+    //     }
+    // }
 
     // delete employee
     public void deleteEmployee(int employeeId){
--- src/FinalProj/access/EmployeeTypeAccess.java
+++ src/FinalProj/access/EmployeeTypeAccess.java
@@ -3,8 +3,6 @@ package FinalProj.access;
 import FinalProj.DatabaseConnection;
 import FinalProj.classes.EmployeeType;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EmployeeTypeAccess{
 
@@ -21,37 +19,37 @@ public class EmployeeTypeAccess{
     }
 
     // get all employee types
-    public List<EmployeeType> getAllEmployeeTypes(){
-        String selectStatement = "select * frpm EmployeeType";
-        List<EmployeeType> employeeTypes = new ArrayList<>();
-        try(Connection conn = DatabaseConnection.connect();
-            Statement stmt = conn.createStatement();
-            ResultSet dataResult = stmt.executeQuery(selectStatement)){
-            while (dataResult.next()){
-                EmployeeType employeeType = new EmployeeType(
-                dataResult.getInt("etID"),
-                dataResult.getString("tName")
-                );
-                employeeTypes.add(employeeType); 
-            }
-        }catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }
-        return employeeTypes;
-    }
+    // public List<EmployeeType> getAllEmployeeTypes(){
+    //     String selectStatement = "select * frpm EmployeeType";
+    //     List<EmployeeType> employeeTypes = new ArrayList<>();
+    //     try(Connection conn = DatabaseConnection.connect();
+    //         Statement stmt = conn.createStatement();
+    //         ResultSet dataResult = stmt.executeQuery(selectStatement)){
+    //         while (dataResult.next()){
+    //             EmployeeType employeeType = new EmployeeType(
+    //             dataResult.getInt("etID"),
+    //             dataResult.getString("tName")
+    //             );
+    //             employeeTypes.add(employeeType); 
+    //         }
+    //     }catch(SQLException ex){
+    //         System.out.println(ex.getMessage());
+    //     }
+    //     return employeeTypes;
+    // }
 
-    // update an existing employee type
-    public void updateEmployeeType(EmployeeType employeeType){
-        String updateStatement = "update EmployeeType set tName = ? where etID = ?";
-        try(Connection conn = DatabaseConnection.connect();
-            PreparedStatement statement = conn.prepareStatement(updateStatement)){
-            statement.setString(1, employeeType.gettName());
-            statement.setInt(2, employeeType.getetID());
-            statement.executeUpdate();
-        }catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
+    // // update an existing employee type
+    // public void updateEmployeeType(EmployeeType employeeType){
+    //     String updateStatement = "update EmployeeType set tName = ? where etID = ?";
+    //     try(Connection conn = DatabaseConnection.connect();
+    //         PreparedStatement statement = conn.prepareStatement(updateStatement)){
+    //         statement.setString(1, employeeType.gettName());
+    //         statement.setInt(2, employeeType.getetID());
+    //         statement.executeUpdate();
+    //     }catch(SQLException ex){
+    //         System.out.println(ex.getMessage());
+    //     }
+    // }
 
     // delete an employee type
     public void deleteEmployeeType(int etID){
