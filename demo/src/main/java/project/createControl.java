a504ee7cd26f6bf08a141908f65833052e171c16
awang154 <awang154@calpoly.edu>
2024-07-26 00:34:41 -0700

    help

--- demo/src/main/java/project/createControl.java
+++ demo/src/main/java/project/createControl.java
@@ -1,9 +1,9 @@
 package project;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+// import java.sql.Connection;
+// import java.sql.PreparedStatement;
+// import java.sql.SQLException;
 
 import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
@@ -16,13 +16,16 @@ public class createControl {
     @FXML
     private TextField phoneNumberField;
     @FXML
+    private TextField createEmployee;
+    @FXML
     private TextField employeeTypeField;
     
     @FXML
     private void createEmployee(){
         String employee = "INSERT INTO Employee(firstName, lastName, phoneNumber, employeeType) VALUES(?, ?, ?, ?)";
+        /* 
         try (Connection conn = DatabaseConnection.connect();
-                PreparedStatement statement = conn.prepareStatement(insertStatement)) {
+             PreparedStatement statement = conn.prepareStatement(insertStatement)) {
             statement.setString(1, firstNameField.getText());
             statement.setString(2, lastNameField.getText());
             statement.setString(3, phoneNumberField.getText());
@@ -34,6 +37,7 @@ public class createControl {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
+            */
     }
 
     @FXML
--- demo/src/main/java/project/scheduleControl.java
+++ demo/src/main/java/project/scheduleControl.java
@@ -1,6 +1,9 @@
 package project;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
@@ -10,6 +13,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class scheduleControl {
+    private Map<String, Label> labelMap;
+
+    @FXML
+    private TextField date;
 
     @FXML
     private TextField ename;
@@ -26,10 +33,75 @@ public class scheduleControl {
 
     @FXML
     private Label monMorn;
+    @FXML
+    private Label monNoon;
+    @FXML
+    private Label monAft;
+
+    @FXML
+    private Label tuesMorn;
+    @FXML
+    private Label tuesNoon;
+    @FXML
+    private Label tuesAft;
+
+    @FXML
+    private Label wedMorn;
+    @FXML
+    private Label wedNoon;
+    @FXML
+    private Label wedAft;
+
+    @FXML
+    private Label thursMorn;
+    @FXML
+    private Label thursNoon;
+    @FXML
+    private Label thursAft;
+
+    @FXML
+    private Label friMorn;
+    @FXML
+    private Label friNoon;
+    @FXML
+    private Label friAft;
 
     public void reg() {
+        labelMap = new HashMap<>();
+        labelMap.put("monMorn", monMorn);
+        labelMap.put("monNoon", monNoon);
+        labelMap.put("monAft", monAft);
+
+        labelMap.put("tuesMorn", tuesMorn);
+        labelMap.put("tuesNoon", tuesNoon);
+        labelMap.put("tuesAft", tuesAft);
+
+        labelMap.put("wedMorn", wedMorn);
+        labelMap.put("wedNoon", wedNoon);
+        labelMap.put("wedAft", wedAft);
+
+        labelMap.put("thursMorn", thursMorn);
+        labelMap.put("thursNoon", thursNoon);
+        labelMap.put("thursAft", thursAft);
+
+        labelMap.put("friMorn", friMorn);
+        labelMap.put("friNoon", friNoon);
+        labelMap.put("friAft", friAft);
+
         String emplName= ename.getText();
-        monMorn.setText(emplName);
+
+        String labelKey = date.getText();
+        Label label = labelMap.get(labelKey);
+
+        if (label != null) {
+            label.setText(emplName);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Label not found: " + labelKey);
+            alert.showAndWait();
+        }
     }
 
 }
