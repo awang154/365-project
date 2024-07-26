cecdb366c887e4c0039b77c5389dca71a6407a53
Ryan Nguyen <rnguye65@calpoly.edu>
2024-07-25 18:38:13 -0700

    new project

---
+++ src/main/java/main/EmployeeTypeAccess.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EmployeeTypeAccess{
+
+    // add a new employee type
+    public void addEmployeeType(EmployeeType employeeType){
+        String insertStatement = "insert into EmployeeType (tName) values (?)";
+        try(Connection conn = DatabaseConnection.connect();
+            PreparedStatement statement = conn.prepareStatement(insertStatement)){
+            statement.setString(1, employeeType.gettName());
+            statement.executeUpdate();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    // get all employee types
+    // public List<EmployeeType> getAllEmployeeTypes(){
+    //     String selectStatement = "select * frpm EmployeeType";
+    //     List<EmployeeType> employeeTypes = new ArrayList<>();
+    //     try(Connection conn = DatabaseConnection.connect();
+    //         Statement stmt = conn.createStatement();
+    //         ResultSet dataResult = stmt.executeQuery(selectStatement)){
+    //         while (dataResult.next()){
+    //             EmployeeType employeeType = new EmployeeType(
+    //             dataResult.getInt("etID"),
+    //             dataResult.getString("tName")
+    //             );
+    //             employeeTypes.add(employeeType); 
+    //         }
+    //     }catch(SQLException ex){
+    //         System.out.println(ex.getMessage());
+    //     }
+    //     return employeeTypes;
+    // }
+
+    // // update an existing employee type
+    // public void updateEmployeeType(EmployeeType employeeType){
+    //     String updateStatement = "update EmployeeType set tName = ? where etID = ?";
+    //     try(Connection conn = DatabaseConnection.connect();
+    //         PreparedStatement statement = conn.prepareStatement(updateStatement)){
+    //         statement.setString(1, employeeType.gettName());
+    //         statement.setInt(2, employeeType.getetID());
+    //         statement.executeUpdate();
+    //     }catch(SQLException ex){
+    //         System.out.println(ex.getMessage());
+    //     }
+    // }
+
+    // delete an employee type
+    public void deleteEmployeeType(int etID){
+        String deleteStatement = "delete from EmployeeType where etID = ?";
+        try(Connection conn = DatabaseConnection.connect();
+             PreparedStatement statement = conn.prepareStatement(deleteStatement)){
+             statement.setInt(1, etID);
+             statement.executeUpdate();
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ src/main/java/main/WorkSchedule.java
@@ -0,0 +1,40 @@
+package main;
+
+public class WorkSchedule {
+    private int schedID;
+    private String date;
+    private int empID;
+
+    // work schedule info
+    public WorkSchedule(int schedID, String date, int empID){
+        this.schedID = schedID;
+        this.date = date;
+        this.empID = empID;
+    }
+
+    // return statements
+    public int getschedID(){
+        return schedID;
+    }
+
+    public void setschedID(int schedID){
+        this.schedID = schedID;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public int getempID(){
+        return empID;
+    }
+
+    public void setempID(int empID){
+        this.empID = empID;
+    }
+}
+
